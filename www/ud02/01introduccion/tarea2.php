fc6bf06409e3b9b940624d4e7fc2694b70eedfe0
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-03-22 18:31:14 +0100

    Tarea ud02/01introduccion/tarea2.php finalizada

--- www/ud02/01introduccion/tarea2.php
+++ www/ud02/01introduccion/tarea2.php
@@ -2,26 +2,47 @@
 
 /**
  * Indica cuál de los siguientes son nombres de variables válidas e inválidos, indica por qué (en comentarios) y corrige los fallos:
-- valor 
+- valor
+=> INVALIDO: Las variables en PHP siempre comienza con el símbolo $.
 - $_N
+=> VALIDO
 - $valor_actual 
+=> VÁLIDO
 - $n
+=> VALIDO
 - $#datos 
+=>INVALIDO: Las variables en PHP deben comenzar siempre por una letra o el símbolo '_'.
 - $valorInicial0
+=> VALIDO
 - $proba,valor 
+=> INVALIDO: No esta permitido el uso de caracteres especiales como ','.
 - $2saldo
+=> INVLAIDO: No es válido que las variables comiencen por un número.
 - $n
+=> VALIDO
 - $meuProblema
+=> VALIDO
 - $meu Problema
+=> INVALIDO: Una variable no puede contener espacios.
 - $echo
+=> VALIDO: a pesar de todo, 'echo' es una palabra reservada en PHP, por lo que lo recomendable sería no utilizarlo para nombrar variables.
 - $m&m
+=> INVALIDO: los nombres de variables en PHP no pueden contener caracteres especiales.
 - $registro
+=> VALIDO
 - $ABC
+=> VALIDO
 - $85 Nome
+=> INVALIDO: Los nombres de las variables no pueden contener espacios. Además, deben comenzar por un guión bajo o una letra.
 - $AAAAAAAAA
+=> VALIDO
 - $nome_apelidos
+=> VALIDO
 - $saldoActual
+=> VALIDO
 - $92
+=> INVALIDO: Los nombres de variables en PHP no pueden comenzar por un número.
 - $*143idade
+=> INVALIDO: No están permitidos caracteres especiales como '*'.
  */
 ?>
