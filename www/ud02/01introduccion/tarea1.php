09bff5f18bd65e39796cbe3552701ce3f702a544
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-03-23 19:05:32 +0100

    Tarea ud02/02arrays/tarea1.php finalizada

--- www/ud02/01introduccion/tarea1.php
+++ www/ud02/01introduccion/tarea1.php
@@ -8,3 +8,4 @@ echo 'Estoy bien, gracias.';
 ??>
 ```
 **/
+// La etiqueta de cierre en la línea 8 es incorrecta. Además, al ser un documente php independiente esta no es necesaria.
--- www/ud02/02arrays/tarea1.php
+++ www/ud02/02arrays/tarea1.php
@@ -1,10 +1,22 @@
 <?php 
 
 //1. Almacena en un array los 10 primeros números pares. Imprímelos cada uno en una línea utilizando los valores contenidos en el array.
+$numerosPares=[];
+for ($i = 1; count($numerosPares) < 10; $i++){
+    if ($i%2==0) {
+        $numerosPares[] = $i;
+    }
+}
+foreach ($numerosPares as $numero){
+    print "<br>$numero";
+}
 
 /* 2. Imprime los valores del array asociativo siguiente usando un foreach:*/
 $v[1]=90;
 $v[10] = 200;
 $v['hola']=43;
 $v[9]='e';
+foreach ($v as $key => $value){
+    print("<br>Clave: $key  => Valor: $value");
+}
 ?>
