70c25f5e4fb8c6bbdb41c13e325797a361088ce2
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-03-25 20:48:56 +0100

    Tarea ud02/03funciones/tarea2.php finalizada

--- www/ud02/03funciones/tarea2.php
+++ www/ud02/03funciones/tarea2.php
@@ -8,5 +8,49 @@
  * Dividimos o número entre 23.
  * Co resto da división anterior, obtemos a letra corresponde na seguinte táboa: 
  */
+function comprobar_nif($nif){
+    //Obtener digitos del dni y convertir esta variable a un tipo numerico;
+    $tablaLetraNif = [
+        0 => "T",
+        1 => "R",
+        2 => "W",
+        3 => "A",
+        4 => "G",
+        5 => "M",
+        6 => "Y",
+        7 => "F",
+        8 => "P",
+        9 => "D",
+        10 => "X",
+        11 => "B",
+        12 => "N",
+        13 => "J",
+        14 => "Z",
+        15 => "S",
+        16 => "Q",
+        17 => "V",
+        18 => "H",
+        19 => "L",
+        20 => "C",
+        21 => "K",
+        22 => "E"
+    ];
+    $digitosDNI = "";
+    $letraDNI = $nif[8];
+    $letraDNI = strtoupper($letraDNI);
+    for ($i = 0; $i < 8; $i++){
+        $digitosDNI = $digitosDNI . $nif[$i];
+    }
+    $digitosDNI =intval($digitosDNI, 10);
+    $moduloDNI = $digitosDNI%23;
+    foreach($tablaLetraNif as $resto => $letra){
+        if ($moduloDNI == $resto && $letraDNI == $letra){
+            echo "El DNI $nif es válido.";
+            return true;
+        }
+    }
+    echo "El DNI $nif no es válido.";
+    return false;
+}
 
 ?>
