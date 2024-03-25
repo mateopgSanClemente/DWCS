ce36817ec40a985f491bc49c5f7097de8eca3ba3
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-03-25 15:39:10 +0100

    Tarea ud02/02arrays/tarea3.php finalizada

--- www/ud02/02arrays/tarea3.php
+++ www/ud02/02arrays/tarea3.php
@@ -5,6 +5,16 @@
 Uso de la función [rand](https://www.php.net/manual/es/function.rand.php). 
 Imprima la matriz creada anteriormente.
 */ 
+$matriz = [];
+while (count($matriz) < 30) {
+    $matriz[] = rand(0, 20);
+}
+$contador=1;
+foreach ($matriz as $elemento){
+    
+    print ("<br>Elemento nº$contador: $elemento");
+    $contador++;
+}
 
 /* 
 2. (Optativo) Cree una matriz con los siguientes datos: 
@@ -15,10 +25,37 @@ Imprima la matriz creada anteriormente.
     - Ordene los elementos de la matriz e imprima la matriz ordenada. 
     - Agregue los siguientes elementos al comienzo de la matriz: `Apple`, `Melon`, `Watermelon`.
 */
-
-
+function imprimirMatrizNumerica($matriz){
+    for($i=0; $i < count($matriz); $i++){
+        print("<br>$matriz[$i]");
+    }
+}
+$simpsons = ["Batman", "Superman", "Krusty", "Bob", "Mel", "Barney"];
+imprimirMatrizNumerica($simpsons);
+//Eliminar última posición de la matriz
+array_pop($simpsons);
+imprimirMatrizNumerica($simpsons);
+//Imprimir última posición donde se encuentra la cadena 'Superman'
+for ($i=0; $i < count($simpsons); $i++){
+    if($simpsons[$i]=="Superman"){
+        print ("<br>Se encontro la cadena 'Superman' en la posición $i.");
+    }
+}
+//Agregue los siguientes elementos al final de la matriz: `Carl`, `Lenny`, `Burns` y `Lisa`.
+array_push($simpsons, "Carl", "Lenny", "Burns", "Lisa");
+imprimirMatrizNumerica($simpsons);
 /*3. (Optativo) Cree una copia de la matriz con el nombre `copia` con elementos del 3 al 5.
     - Agregue el elemento 'Pera' al final de la matriz. */ 
+function copiarArray ($array){
+    $copiar = [];
+    for ($i=3; $i <= 5; $i++){
+        $copia[] = $array[$i];
+    }
+    $copia[] = "Pera";
+    return $copia;
+}
 
+$copia = copiarArray($simpsons);
+var_dump($copia);
 
 ?>
