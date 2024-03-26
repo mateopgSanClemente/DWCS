eedcd74ffa0ecee589e48696c4640f283b305bb4
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-03-26 17:38:40 +0100

    Tarea ud02/04formularios/tarea2_optativa.php: Finalizado el script php que muestra los productos seleccionados, cantidad y precio total.

--- www/ud02/04formularios/tarea2_optativo.php
+++ www/ud02/04formularios/tarea2_optativo.php
@@ -19,7 +19,7 @@
                         <tr>
                             <td>
                                 <label for="cocaCola">Coca Cola</label>
-                                <input type="checkbox" name="cocaCola" id="cocaCola">
+                                <input type="checkbox" name="checkbox[]" id="cocaCola" value="Coca Cola">
                             </td>
                             <td>1 €</td>
                             <td>
@@ -29,7 +29,7 @@
                         <tr>
                             <td>
                                 <label for="pepsiCola">Pepsi Cola</label>
-                                <input type="checkbox" name="pepsiCola" id="pepsiCola">
+                                <input type="checkbox" name="checkbox[]" id="pepsiCola" value="Pepsi Cola">
                             </td>
                             <td>0,80 €</td>
                             <td>
@@ -39,7 +39,7 @@
                         <tr>
                             <td>
                                 <label for="fantaNaranaja">Fanta Naranaja</label>
-                                <input type="checkbox" name="fantaNaranja" id="fantaNaranja">
+                                <input type="checkbox" name="checkbox[]" id="fantaNaranja" value="Fanta Naranja">
                             </td>
                             <td>0,90 €</td>
                             <td>
@@ -49,7 +49,7 @@
                         <tr>
                             <td>
                                 <label for="trinaManzana">Trina Manzana</label>
-                                <input type="checkbox" name="trinaManzana" id="trinaManzana">
+                                <input type="checkbox" name="checkbox[]" id="trinaManzana" value="Trina Manzana">
                             </td>
                             <td>1,10 €</td>
                             <td>
@@ -58,12 +58,12 @@
                         </tr>
                     </tbody>
                 </table>
+                <input type="submit" value="Solicitar" name="solicitar" id="solicitar">
             </form>
-
         </div>
         <?php 
-/*
-Crea un formulario para solicitar una de las siguientes bebidas:
+    /*
+    Crea un formulario para solicitar una de las siguientes bebidas:
 
     Bebida|PVP
     :-|:-:
@@ -80,7 +80,37 @@ Crea un formulario para solicitar una de las siguientes bebidas:
     Puedes utilizar sentencias `switch` o `if`.
     */
     //Aquí va el código PHP que muestra la información solicitada.
-            
+            $precioCola = 1;
+            $precioPepsi = 0.80;
+            $precioFantaNaranja = 0.90;
+            $precioTrinaManzana = 1.10;
+            if (!empty($_POST["checkbox"])){
+                $cantidadCola = $_POST["cantidadCola"];
+                $cantidadPepsi = $_POST["cantidadPepsi"];
+                $cantidadFantaNaranja = $_POST["cantidadFantaNaranja"];
+                $cantidadTrinaManzana = $_POST["cantidadTrinaManzana"];
+                $resultado = "Pediste ";
+                foreach ($_POST["checkbox"] as $valor){
+                    if ($valor == "Coca Cola"){
+                        $resultado = $resultado . $cantidadCola . " " . $valor;
+                    } else if ($valor == "Pepsi Cola"){
+                        $resultado = $resultado . $cantidadPepsi . " " . $valor;
+                    } else if ($valor == "Fanta Naranja"){
+                        $resultado = $resultado . $cantidadFantaNaranja . " " . $valor;
+                    } else if ($valor == "Trina Manzana"){
+                        $resultado = $resultado . $cantidadTrinaManzana . " " . $valor;
+                    }
+                    $resultado = $resultado . ", ";
+                    /*Como puedo conseguir que, en caso de seleccionar varios productos y estar en el último elemento, aparezca una 'y' entre el penúltimo y último elemento??
+                        POSIBLE SOLUCIÓ: Usar apuntadores para recorrer el array
+                    */
+                }
+                $resultado = $resultado . "precio total a pagar ";
+                $precioTotal = $precioCola * $cantidadCola + $precioPepsi * $cantidadPepsi + $precioFantaNaranja * $cantidadFantaNaranja + $precioTrinaManzana * $cantidadTrinaManzana;
+                $precioFormateado = number_format($precioTotal,2 , ",", ".") . "€";
+                $resultado = $resultado . $precioFormateado . ".";
+                echo "$resultado";
+            }
         ?>
     </body>
 </html>
