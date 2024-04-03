a69ad2435fbb48abf0a2d9dcc3f45847ff33d490
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-04-03 18:00:24 +0200

    Se incluyeron las funciones 'crear_tabla_historico()', 'crear_tabla_administradores()', 'registrar_donante()', 'mostrar_donante()', 'registrar_donancion()' y 'eliminar_donante()' del fichero 'base_datos.php'.

--- www/ud03/donaciones/lib/base_datos.php
+++ www/ud03/donaciones/lib/base_datos.php
@@ -41,9 +41,9 @@
             nombre VARCHAR(50) NOT NULL,
             apellidos VARCHAR(100) NOT NULL,
             edad INT NOT NULL CHECK (edad>=18 AND edad<=120),
-            grupoSanguineo VARCHAR(3) CHECK (grupoSanguineo IN ('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-')),
-            codigoPostas INT CHECK (LENGTH(codigoPostal)=5),
-            telefonoMovil INT CHECK (LENGTH(telefonoMovil)=9)
+            grupoSanguineo VARCHAR(3) NOT NULL CHECK (grupoSanguineo IN ('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-')),
+            codigoPostas INT NOT NULL CHECK (LENGTH(codigoPostal)=5),
+            telefonoMovil INT NOT NULL CHECK (LENGTH(telefonoMovil)=9)
         );";
         try{
             ejecutar_consulta($conPDO, $sql);
@@ -52,5 +52,74 @@
         }
     }
 
-    function crear_tabla_historico($conPDO){}
+    function crear_tabla_historico($conPDO){
+        $sql="CREATE TABLE IF NOT EXISTS historicos(
+            id INT PRIMARY KEY,
+            idDonante INT,
+            FOREIGN KEY (idDonante) REFERENCES donantes(id),
+            fechaDonacion DATE,
+            fechaProxDonacion DATE AS (DATE_ADD(fechaDonacion, INTERVAL 4 MONTH))
+            );";
+        try{
+            ejecutar_consulta($conPDO, $sql);
+        }catch(PDOException $e){
+            echo"Se produjo un error a la hora de crear la tabla: ".$e->getMessage();
+        }
+    }
+
+    function crear_tabla_administradores($conPDO){
+        $sql="CREATE TABLE IF NOT EXISTS administradores(
+            nombreUsuario VARCHAR(50) NOT NULL PRIMARY KEY,
+            pass VARCHAR(200) NOT NULL
+            );";
+        ejecutar_consulta($conPDO, $sql);
+    }
+
+    function registrar_donante($conPDO, $nombre, $apellidos, $edad, $grupoSanguineo, $codigoPostal, $telefonoMovil){
+        try{
+            $sql="INSERT INTO donantes (nombre, apellidos, edad, grupoSanguineo, codigoPostal, telefonoMovil)
+            VALUES (:nombre, :apellido, :edad, :grupoSanguineo, :codigoPostal, :telefonoMovil);";
+            $stmt = $conPDO->prepare($sql);
+            $stmt->bindParam(":nombre", $nombre);
+            $stmt->bindParam(":apellidos", $apellidos);
+            $stmt->bindParam(":edad", $edad);
+            $stmt->bindParam(":grupoSanguineo", $grupoSanguineo);
+            $stmt->bindParam(":codigoPostal", $codigoPostal);
+            $stmt->bindParam(":telefonoMovil", $telefonoMovil);
+            $stmt->execute();
+        }catch(PDOException $e){
+            echo"Hubo un error a hora de crear la tabla 'donantes': " . $e->getMessage();
+        }
+    }
+
+    function mostrar_donantes($conPDO){
+        try{
+            $sql="SELECT * FROM donantes;";
+            ejecutar_consulta($conPDO, $sql);
+        } catch(PDOException $e){
+            echo"Se produjo un error a la hora de seleccionar los datos de la tabla 'donantes': ".$e->getMessage();
+        }
+    }
+
+    function registrar_donacion($conPDO, $idDonante){
+        try{
+            $sql="INSERT INTO historicos (idDonante, fechaDonacion)
+            VALUES (:idDonante, :fechaDonacion);";
+            $stmt=$conPDO->prepare($sql);
+            $stmt->bindParam(":idDonante", $idDonante);
+            $stmt->bindParam(":fechaDonacion", $fechaDonacion);
+            $stmt->execute();
+        }catch(PDOException $e){
+            echo"Se produjo un error a la hora de registrar la donacion: ".$e->getMessage();
+        }
+    }
+
+    function eliminar_donante($conPDO, $idDonante){
+        try{
+            $sql="DELETE FROM donantes WHERE id=$idDonante;";
+            ejecutar_consulta($conPDO, $sql);
+        }catch(PDOException $e){
+            echo"Se produjo un error a la hora de eliminar al usuario con id ".$idDonante.": ". $e->getMessage();
+        }
+    }
 ?>
