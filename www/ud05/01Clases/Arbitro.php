ac40dcd05d38dbad4ba18b73c8f720f4533b70f3
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-04-15 14:51:49 +0200

    Se probaron los métodos y propiedades de las clases Jugador y Arbitro en sus respectivos ficheros.

--- www/ud05/01Clases/Arbitro.php
+++ www/ud05/01Clases/Arbitro.php
@@ -1,26 +1,50 @@
 <?php
-class Jugador extends Participante{
-    private int $anhosTrabajando;
+    require_once "Participante.php";
+    class Arbitro extends Participante{
+        private int $anhosTrabajando;
 
-    public function __construct(string $nombre, int $edad, int $anhosTrabajando){
-        if($anhosTrabajando>=0){
-            parent::__construct($nombre, $edad);
-            $this->anhosTrabajando = $anhosTrabajando;
-        } else {
-            echo "El valor introducido para los años trabajados no es correcta, debe ser 0 o superior.";
+        public function __construct(string $nombre, int $edad, int $anhosTrabajando){
+            if($anhosTrabajando>=0){
+                parent::__construct($nombre, $edad);
+                $this->anhosTrabajando = $anhosTrabajando;
+            } else {
+                echo "El valor introducido para los años trabajados no es correcta, debe ser 0 o superior.";
+            }
+        }
+
+        public function setAnhosTrabajando(int $anhosTrabajando){
+            if($anhosTrabajando>=0){
+                $this->anhosTrabajando = $anhosTrabajando;
+            } else {
+                echo "El valor introducido para los años trabajados no es correcta, debe ser 0 o superior.";
+            }
+        }
+
+        public function getAnhosTrabajando(){
+            return $this->anhosTrabajando;
         }
     }
 
-    public function setAnhosTrabajando(int $anhosTrabajando){
-        if($anhosTrabajando>=0){
-            $this->anhosTrabajando = $anhosTrabajando;
-        } else {
-            echo "El valor introducido para los años trabajados no es correcta, debe ser 0 o superior.";
-        }
-    }
+    $arbitro1 = new Arbitro ("Ana", 34, 9);
+    $arbitro2 = new Arbitro ("Santi", 45, 8);
 
-    public function getAnhosTrabajando(){
-        return $this->anhosTrabajando;
-    }
-}
+    echo "<br>". $arbitro1->getNombre();
+    $arbitro1->setNombre("Ana 2");
+    echo "<br>". $arbitro1->getNombre();
+    echo "<br>". $arbitro1->getEdad();
+    $arbitro1->setEdad(99);
+    echo "<br>". $arbitro1->getEdad();
+    echo "<br>". $arbitro1->getAnhosTrabajando();
+    $arbitro1->setAnhosTrabajando(200);
+    echo "<br>". $arbitro1->getAnhosTrabajando();
+
+    echo "<br>". $arbitro2->getNombre();
+    $arbitro2->setNombre("Santi 2");
+    echo "<br>". $arbitro2->getNombre();
+    echo "<br>". $arbitro2->getEdad();
+    $arbitro2->setEdad(99);
+    echo "<br>". $arbitro2->getEdad();
+    echo "<br>". $arbitro2->getAnhosTrabajando();
+    $arbitro2->setAnhosTrabajando(300);
+    echo "<br>". $arbitro2->getAnhosTrabajando();
 ?>
--- www/ud05/01Clases/Jugador.php
+++ www/ud05/01Clases/Jugador.php
@@ -10,11 +10,12 @@
         }
 
         public function setPosicion(string $posicion){
-            if(in_array($posicion, $posicionesPermitidas)){
+            if(in_array($posicion, $this->posicionesPermitidas)){
+                $posicion = strtolower($posicion);
                 $this->posicion = $posicion;
             } else {
                 echo "<br>La posicion '$posicion' no es válida. Solo es posible dar las siguientes posiciones:";
-                foreach($posicionesPermitidas as $p){
+                foreach($this->posicionesPermitidas as $p){
                     echo "<br>\t-$p";
                 }
             }
@@ -24,4 +25,28 @@
             return $this->posicion;
         }
     }
+
+    $jugador1 = new Jugador("Juan", 34, "portero");
+    $jugador2 = new Jugador("Pedro", 56, "caballo");
+    $jugador2 = new Jugador("Pedro", 56, "delantero");
+
+    echo "<br>". $jugador1->getNombre();
+    echo "<br>". $jugador1->getEdad();
+    echo "<br>". $jugador1->getPosicion();
+    $jugador1->setNombre("Juan 2");
+    $jugador1->setEdad(567);
+    $jugador1->setPosicion("defensa");
+    echo "<br>". $jugador1->getNombre();
+    echo "<br>". $jugador1->getEdad();
+    echo "<br>". $jugador1->getPosicion();
+
+    echo "<br>". $jugador2->getNombre();
+    echo "<br>". $jugador2->getEdad();
+    echo "<br>". $jugador2->getPosicion();
+    $jugador2->setNombre("Pedro 2");
+    $jugador2->setEdad(568);
+    $jugador2->setPosicion("centrocampista");
+    echo "<br>". $jugador2->getNombre();
+    echo "<br>". $jugador2->getEdad();
+    echo "<br>". $jugador2->getPosicion();
 ?>
