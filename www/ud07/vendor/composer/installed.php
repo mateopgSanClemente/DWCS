783e4bb6b032203df622e525699bb546b2e086e4
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-06-17 14:15:34 +0200

    Preparando ud7

---
+++ www/ud07/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7f0bfec25934934ad6bb3879354ceb47e335abc0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7f0bfec25934934ad6bb3879354ceb47e335abc0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => '3.0.0',
+            'version' => '3.0.0.0',
+            'reference' => 'fe5ea303b0887d5caefd3d431c3e61ad47037001',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/debug' => array(
+            'pretty_version' => 'v4.4.44',
+            'version' => '4.4.44.0',
+            'reference' => '1a692492190773c5310bc7877cb590c04c2f05be',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/debug',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ www/ud08/symfony_projects/src/Controller/LibrosController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use function Symfony\Component\String\u;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+// MAL? use Symfony\Component\Cache\CacheItemInterface;
+// Altearnativa? (Non da erro, así que 👍)
+use Psr\Cache\CacheItemInterface;
+// Alternativa? NO-> use Symfony\Contracts\Cache\ItemInterface;
+
+class LibrosController extends AbstractController
+{
+
+    #[Route('/')]
+    public function homepage()
+    {
+        return $this->render('libros/homepage.html.twig', [
+            'title' => 'Inicio',
+        ]);
+    }
+
+    #[Route('/sobre')]
+    public function sobre()
+    {
+
+        $afeccions = [
+            'Videoxogos',
+            'Programar',
+            'Ler',
+        ];
+
+        return $this->render('libros/sobre.html.twig', [
+            'title' => 'Sobre esta web',
+            'nome' => 'Manuel',
+            'n_exemplares' => 128,
+            'mail' => 'manuel@mail.com',
+            'afeccions' => $afeccions,
+        ]);
+    }
+
+    #[Route('/explorar/{xenero}')]
+    public function fasvs(
+        HttpClientInterface $httpClient,
+        CacheInterface $cache,
+        string $xenero = null
+        ): Response
+    {
+        if ($xenero) {
+            $title = u(str_replace('-', ' ', $xenero))->title(true);
+            $libros = '';
+        } else {
+            $title = 'Todos os xéneros';
+            $libros = $cache->get('libros_data', function(CacheItemInterface $cacheItem) use ($httpClient) {
+                $cacheItem->expiresAfter(10);
+                $response = $httpClient->request('GET', 'https://gist.githubusercontent.com/sanket143/5346f04575851a5228b8c5c1e99496af/raw/44dd530d524fa8e467d61680d0c713736220170f/books.json');
+                return $response->toArray();
+            });
+        }
+
+        return $this->render('libros/xeneros.html.twig', [
+            'title' => $title,
+            'xenero' => $xenero,
+            'libros' => $libros
+        ]);
+
+        return new Response('Xénero: ' . $title);
+    }
+
+    #[Route('/abreviaturas')]
+    public function abreviaturas()
+    {
+        $xeneros = [
+            'CMD - Comedia',
+            'DRM - Drama',
+            'INF - Infantil',
+            'TIC - Tecnoloxías Información e Comunicación',
+            'XVN - Xuvenil',
+        ];
+
+        $tipos = [
+            ['abreviatura' => 'bade', 'expansion' => 'Banda Deseñada'],
+            ['abreviatura' => 'cate', 'expansion' => 'Catálogo de exposición'],
+            ['abreviatura' => 'ensa', 'expansion' => 'Ensaio'],
+            ['abreviatura' => 'nvl', 'expansion' => 'Novela'],
+            ['abreviatura' => 'poes', 'expansion' => 'Poesía'],
+        ];
+
+        return $this->render('libros/abreviaturas.html.twig', [
+            'title' => 'Abreviaturas da colección',
+            'xeneros' => $xeneros,
+            'tipos' => $tipos,
+        ]);
+    }
+}
